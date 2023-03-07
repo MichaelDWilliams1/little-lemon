a9b98dd9e2b4ed73ff84ed40badf617fa95513fd
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2023-03-06 22:48:48 -0800

    made testimonials section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Specials from './components/Specials';
+import Testimonials from './components/Testimonials';
 function App() {
   return (
     <div className="">
       <Navigation />
       <Header />
       <Specials />
+      <Testimonials />
     </div>
   );
 }
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <section className='bg-green-900 h-96 font-spartan flex flex-col items-center space-y-10'>
+        <h3 className='text-8xl text-yellow-300 mt-16'>Testimonials</h3>
+        <p className='text-7xl w-1000 text-white'><span className='text-yellow-300'>"</span>Service was amazing! Me and my family will be coming back!</p>
+
+    </section>
+  )
+}
+
+export default Testimonials
